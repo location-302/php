c9a409af0258f9eb7afb337e39d7dd8f128ea2fb
dmitrik <dmitrik@ironsrc.com>
2015-05-11 09:38:47 +0300

    remove url encode

--- location-302.php
+++ location-302.php
@@ -1,9 +1,8 @@
 <?php
 function location302Url($id, $secret, $url) {
-	$redirectUrl = urlencode($url);
 	$serviceUrl = "http://302-location.com";
 
-	$params = array("id" => $id, "url" => $redirectUrl);
+	$params = array("id" => $id, "url" => $url);
 	$params["token"] = hash("sha256", $secret.http_build_query($params));
 
 	return $serviceUrl."/?".http_build_query($params);
