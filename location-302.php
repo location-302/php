fa32b73d1d46e33c41d32efadeb9cff17306c14b
dmitrik <dmitrik@ironsrc.com>
2015-05-21 09:26:15 +0300

    substring token

--- location-302.php
+++ location-302.php
@@ -2,8 +2,9 @@
 function location302Url($id, $secret, $url) {
 	$serviceUrl = "http://302-location.com";
 
-	$params = array("id" => $id, "url" => $url);
-	$params["token"] = hash("sha256", $secret.http_build_query($params));
+	$params = array("i" => $id, "u" => $url);
+	$params["t"] = hash("sha256", $secret.http_build_query($params));
+	$params["t"] = substr($params["t"], 0, 4);
 
 	return $serviceUrl."/?".http_build_query($params);
 }
